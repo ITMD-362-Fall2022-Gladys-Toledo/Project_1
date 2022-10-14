b97c3c992dd6dc8238ba24b76d504f7591e0f87e
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2022-10-14 03:08:46 -0500

    fixing indentation

--- js/site.js
+++ js/site.js
@@ -1,12 +1,12 @@
 function checkForm() {
     var firstName = document.forms["mailingListForm"]["first_name"].value
     var lastName = document.forms["mailingListForm"]["last_name"].value
-	var email = document.forms["mailingListForm"]["email_data"].value
-	if((!firstName) || (!lastName) || (!email)) {
-		alert("Please complete all the necessary fields!");
-		return false;
-	}
-	else {
-		alert("You have successfully registered for our mailing list!")
-	}
+    var email = document.forms["mailingListForm"]["email_data"].value
+    if ((!firstName) || (!lastName) || (!email)) {
+        alert("Please complete all the necessary fields!");
+        return false;
+    }
+    else {
+        alert("You have successfully registered for our mailing list!")
+    }
 }
